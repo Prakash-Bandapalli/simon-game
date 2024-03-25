4fcd23576a36489e8744f91b68d7ea028887a69d
Prakash <prakash.bandapalli@gmail.com>
2024-03-25 23:03:41 +0530

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
+$(".button-92").click(function () {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
@@ -14,51 +14,42 @@ $(document).keypress(function() {
   }
 });
 
-$(".btn").click(function() {
-
+$(".btn").click(function () {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
 
   playSound(userChosenColour);
   animatePress(userChosenColour);
 
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
-
 function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
 
-    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-
-      console.log("success");
-
-      if (userClickedPattern.length === gamePattern.length){
-        setTimeout(function () {
-          nextSequence();
-        }, 1000);
-      }
-
-    } else {
-
-      console.log("wrong");
-
-      playSound("wrong");
-
-      $("body").addClass("game-over");
+    if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function () {
-        $("body").removeClass("game-over");
-      }, 200);
-
-      $("#level-title").text("Game Over, Press Any Key to Restart");
-
-      //2. Call startOver() if the user gets the sequence wrong.
-      startOver();
+        nextSequence();
+      }, 1000);
     }
+  } else {
+    console.log("wrong");
 
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
 }
 
 function nextSequence() {
-
   userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
@@ -67,7 +58,10 @@ function nextSequence() {
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
-  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
   playSound(randomChosenColour);
 }
 
@@ -83,10 +77,7 @@ function animatePress(currentColor) {
   }, 100);
 }
 
-//1. Create a new function called startOver().
 function startOver() {
-
-  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
   level = 0;
   gamePattern = [];
   started = false;
